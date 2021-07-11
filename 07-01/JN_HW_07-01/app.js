bbec75b639cfdee1ff41d1a393548cb020314846
Your Name <you@example.com>
2021-07-11 21:40:42 +0300

    'pabaigtas'

--- 07-01/JN_HW_07-01/app.js
+++ 07-01/JN_HW_07-01/app.js
@@ -17,22 +17,31 @@ function lof(){
 }
 ///////////////////
 ///////////////////EMAIL/PHONE NUMBER
+function email(inputtxt){
+    let mail = /^[^\s@]+@[^\s@]+$/;
+    if (inputtxt.value.match(mail))
+    {
+        return true;
+    }
+    else
+    {
+        alert('Please provide a valid email address');
+        return false;
+    }
+}
 
-// /^[^\s@]+@[^\s@]+$/
-
-
-// function ValidateEmail(mail)
-// {
-//     if (/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(document.getElementById('staticEmail').value))
-//     {
-//         return (true)
-//     }
-//     alert("You have entered an invalid email address!")
-//     return (false)
-// }
-
-
-
+function phonenumber(inputtxt){
+    let num = /^\d{10}$/;
+    if(inputtxt.value.match(num))
+    {
+        return true;
+    }
+    else
+    {
+        alert("Please provide a valid Phone Number");
+        return false;
+    }
+}
 ///////////////////
 ///////////////////BLOCK/UNBLOCK
 function block(){
@@ -84,23 +93,3 @@ function reset(){
 }
 ///////////////////
 ///////////////////MODAL
-// Get the modal
-let modal = document.getElementById("myModal");
-// Get the button that opens the modal
-let btn = document.getElementById("myBtn");
-// Get the <span> element that closes the modal
-let span = document.getElementsByClassName("close")[0];
-// When the user clicks on the button, open the modal
-btn.onclick = function() {
-    modal.style.display = "block";
-}
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-    modal.style.display = "none";
-}
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
